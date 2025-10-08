2a131b2117fcd79e641b510087859b0c45556f36
adamswan <melyling@sina.com>
2025-10-08 17:10:15 +0800

    feat: 初始化项目

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+// 状态的不可变
+function App() {
+  const [info, setInfo] = useState({
+    name: "高圆圆",
+    age: 18,
+  });
+
+  const handleClick = () => {
+    const newInfo = { ...info };
+    newInfo.age = newInfo.age + 1;
+    setInfo(newInfo);
+  };
+
+  return (
+    <>
+      <p>
+        {info.name} - {info.age}
+      </p>
+      <button onClick={handleClick}>点我修改年龄</button>
+    </>
+  );
+}
+
+export default App;
